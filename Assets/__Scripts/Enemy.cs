63fafce3f9b28b55fdccb180c1e0ff37a0db65c5
vishnu292 <47337198+vishnu292@users.noreply.github.com>
2019-12-06 04:40:54 -0500

    skeletos initial movement

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    protected static Vector3[] directions = new Vector3[] { Vector3.right, Vector3.up, Vector3.left, Vector3.down };
+
+    [Header("Set in Inspector: Enemy")]
+    public float maxHealth = 1;
+
+    [Header("Set Dynamically: Enemy")]
+    public float health;
+
+    protected Animator anim;
+    protected Rigidbody rigid;
+    protected SpriteRenderer sRend;
+
+    protected virtual void Awake()
+    {
+        health = maxHealth;
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        sRend = GetComponent<SpriteRenderer>();
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/__Scripts/Skeletos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeletos : Enemy
+{
+    [Header("Set in Inpsector: Skeletos")]
+    public int speed = 2;
+    public float timeThinkMin = 1f;
+    public float timeThinkMax = 4f;
+
+    [Header("Set Dynamically: Skeletos")]
+    public int facing = 0;
+    public float timeNextDecision;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= timeNextDecision)
+        {
+            DecideDirection();
+        }
+        rigid.velocity = directions[facing] * speed;
+    }
+    void DecideDirection()
+    {
+        facing = Random.Range(0, 4);
+        timeNextDecision = Time.time + Random.Range(timeThinkMin, timeThinkMax);
+    }
+}
