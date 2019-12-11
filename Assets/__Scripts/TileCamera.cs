dfc47a9dde19d7d45008f85ef0f9f7d0b98b86be
vishnu292 <47337198+vishnu292@users.noreply.github.com>
2019-12-11 04:47:57 -0500

    Scene Hat

--- Assets/__Scripts/TileCamera.cs
+++ Assets/__Scripts/TileCamera.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class TileSwap
+{
+    public int tileNum;
+    public GameObject swapPrefab;
+    public GameObject guaranteedItemDrop;
+    public int overrideTileNum = -1;
+}
+
+
 public class TileCamera : MonoBehaviour
 {
     static private int W, H;
@@ -18,9 +28,17 @@ public class TileCamera : MonoBehaviour
     public Texture2D mapTiles;
     public TextAsset mapCollisions;
     public Tile tilePrefab;
+    public int defaultTileNum;
+    public List<TileSwap> tileSwaps;
+
+    private Dictionary<int, TileSwap> tileSwapDict;
+    private Transform enemyAnchor, itemAnchor;
 
     void Awake() {
         COLLISIONS = Utils.RemoveLineEndings(mapCollisions.text);
+        PrepareTileSwapDict();
+        enemyAnchor = (new GameObject("Enemy Anchor")).transform;
+        itemAnchor = (new GameObject("Item Anchor")).transform;
         LoadMap();
         
     }
@@ -51,6 +69,7 @@ public class TileCamera : MonoBehaviour
                 else {
                     MAP[i, j] = int.Parse(tileNums[i], hexNum);
                 }
+                CheckTileSwaps(i, j);
             }
         }
         print("Parsed " + SPRITES.Length + " sprites.");
@@ -74,6 +93,54 @@ public class TileCamera : MonoBehaviour
                 }              
             }
         }
+    }
+
+    void PrepareTileSwapDict()
+    {
+        tileSwapDict = new Dictionary<int, TileSwap>();
+        foreach(TileSwap ts in tileSwaps)
+        {
+            tileSwapDict.Add(ts.tileNum, ts);
+        }
+    }
+
+    void CheckTileSwaps(int i, int j)
+    {
+        int tNum = GET_MAP(i, j);
+        if (!tileSwapDict.ContainsKey(tNum)) return;
+
+        TileSwap ts = tileSwapDict[tNum];
+        if(ts.swapPrefab != null)
+        {
+            GameObject go = Instantiate(ts.swapPrefab);
+            Enemy e = go.GetComponent<Enemy>();
+            if(e != null)
+            {
+                go.transform.SetParent(enemyAnchor);
+            }
+            else
+            {
+                go.transform.SetParent(itemAnchor);
+            }
+            go.transform.position = new Vector3(i, j, 0);
+            if (ts.guaranteedItemDrop != null)
+            {
+                if(e != null)
+                {
+                    e.guaranteedItemDrop = ts.guaranteedItemDrop;
+                }
+            }
+        }
+        if (ts.overrideTileNum == -1)
+        {
+            SET_MAP(i, j, defaultTileNum);
+
+        }
+        else
+        {
+            SET_MAP(i, j, ts.overrideTileNum);
+        }
+
     }
 
     static public int GET_MAP(int x, int y) {
